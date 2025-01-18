73154cb8430ad17eefddb7b0ca7bc439577795bc
allogic <mial.burg@gmail.com>
2025-01-18 01:44:26 +0100

    Unfinished business

--- core/core_heap.asm
+++ core/core_heap.asm
@@ -1,22 +1,23 @@
-INCLUDE core_console.inc
 INCLUDE core_heap.inc
 INCLUDE core_macros.inc
 
 PAGE_SIZE EQU 1000h
 
-MEM_COMMIT EQU 1000h
+MEM_COMMIT  EQU 1000h
 MEM_RESERVE EQU 2000h
 MEM_RELEASE EQU 8000h
 
 PAGE_READWRITE EQU 4h
 
+extern printf : proc
+
 extern VirtualAlloc : proc
-extern VirtualFree : proc
+extern VirtualFree  : proc
 
 .data
 
-g_heap_size qword ?
-g_heap_leak_format_string byte "%zu bytes not freed", 10, 0
+g_heap_size               qword 0
+g_heap_leak_format_string byte  "%zu bytes not freed", 10, 0
 
 .code
 
@@ -27,7 +28,8 @@ heap_initialize proc
 
 	FUNCTION_PROLOGUE
 
-	mov g_heap_size, 0 ; Reset heap size
+	; Reset heap size
+	mov       g_heap_size, 0 ; Set allocated heap size back to 0
 
 	FUNCTION_EPILOGUE
 
@@ -42,27 +44,34 @@ heap_alloc proc block_size:qword
 
 	FUNCTION_PROLOGUE
 
-	mov rbx, rcx ; Temporary to hold block size
+	; Temporary variables
+	mov       r12, rcx ; Temporary to hold block size
 
-	add rbx, SIZEOF qword       ; Add block primitive size
-	ALIGN_UP_REG rbx, PAGE_SIZE ; Align size up to the nearest page boundary
+	; Compute number of bytes to allocate
+	add       r12, SIZEOF qword ; Add block primitive size
+	ALIGN_UR  r12, PAGE_SIZE    ; Align size up to the nearest page boundary
 
 	; Alloc virtual block
-	CALL_PROLOGUE_IMM 0h
-	xor rcx, rcx                      ; [ARG0] lpAddress
-	mov rdx, rbx                      ; [ARG1] dwSize
-	mov r8, MEM_COMMIT OR MEM_RESERVE ; [ARG2] flAllocationType
-	mov r9, PAGE_READWRITE            ; [ARG3] flProtect
-	call VirtualAlloc
-	CALL_EPILOGUE_IMM 0h
+	sub       rsp, 28h                      ; Allocate shadow space and align stack
+	mov       r9, PAGE_READWRITE            ; [ARG3] flProtect
+	mov       r8, MEM_COMMIT OR MEM_RESERVE ; [ARG2] flAllocationType
+	mov       rdx, r12                      ; [ARG1] dwSize
+	xor       rcx, rcx                      ; [ARG0] lpAddress
+	call      VirtualAlloc                  ; Virtual alloc
+	add       rsp, 28h                      ; Restore stack
 
 IFDEF DEBUG
-	add g_heap_size, rbx     ; Add block size to overall size
-	mov qword ptr [rax], rbx ; Store block size in block
-	add rax, SIZEOF qword    ; Increment block past block size
+
+	; Store heap size
+	add       g_heap_size, r12     ; Add block size to overall size
+	mov       qword ptr [rax], r12 ; Store block size in block
+	add       rax, SIZEOF qword    ; Increment block past block size
+
 ENDIF ; DEBUG
 
 	FUNCTION_EPILOGUE
+
+	ret
 
 heap_alloc endp
 
@@ -73,16 +82,23 @@ heap_free proc block:qword
 
 	FUNCTION_PROLOGUE
 
+	; Temporary variables
+	mov       r12, rcx                            ; Temporary to hold block
+	mov       r13, qword ptr [r12 - SIZEOF qword] ; Temporary to hold block size
+
 	; Free virtual block
-	CALL_PROLOGUE_IMM 0h
-	xor rdx, rdx         ; [ARG1] dwSize
-	mov r8, MEM_RELEASE  ; [ARG2] dwFreeType
-	call VirtualFree
-	CALL_EPILOGUE_IMM 0h
+	sub       rsp, 28h        ; Allocate shadow space and align stack
+	mov       r8, MEM_RELEASE ; [ARG2] dwFreeType
+	xor       rdx, rdx        ; [ARG1] dwSize
+	mov       rcx, r12        ; [ARG0] lpAddress
+	call      VirtualFree     ; Virtual free
+	add       rsp, 28h        ; Restore stack
 
 IFDEF DEBUG
-	mov rbx, qword ptr [rcx - SIZEOF qword] ; Temporary to hold block size
-	sub g_heap_size, rbx                    ; Subtract block size from overall size
+
+	; Restore heap size
+	sub       g_heap_size, r13 ; Subtract block size from overall size
+
 ENDIF ; DEBUG
 
 	FUNCTION_EPILOGUE
@@ -99,17 +115,15 @@ heap_validate proc
 	FUNCTION_PROLOGUE
 
 	; Check heap size
-	cmp g_heap_size, 0
-	jz no_leak_found
+	cmp       g_heap_size, 0 ; Compare allocated heap size
+	je        no_leak_found  ; Jump to end if no leak was found
 
 	; Print leaked byte count
-	CALL_PROLOGUE_IMM 8h
-	lea rcx, g_heap_leak_format_string ; [ARG0] format
-	mov rdx, 1                         ; [ARG1] num_args
-	mov rax, g_heap_size
-	mov qword ptr [rsp], rax           ; [ARG2] variadic
-	call console_log
-	CALL_EPILOGUE_IMM 8h
+	sub       rsp, 28h                       ; Allocate shadow space and align stack
+	mov       rdx, g_heap_size               ; [ARG1] variadic
+	lea       rcx, g_heap_leak_format_string ; [ARG0] format
+	call      printf                         ; Print formatted
+	add       rsp, 28h                       ; Restore stack
 
 no_leak_found:
 
--- engine/engine_main.asm
+++ engine/engine_main.asm
@@ -1,4 +1,3 @@
-INCLUDE core_console.inc
 INCLUDE core_heap.inc
 INCLUDE core_macros.inc
 
@@ -6,17 +5,6 @@ INCLUDE engine_main.inc
 INCLUDE engine_window.inc
 
 extern _CRT_INIT : proc
-
-.data
-
-g_fld_ctrl   word  37Fh
-g_mxcsr_ctrl dword 1F80h
-
-; TODO
-g_format_string_0 byte "zero", 10, 0
-g_format_string_1 byte "first:%zu", 10, 0
-g_format_string_2 byte "first:%zu second:%zu", 10, 0
-g_format_string_3 byte "first:%zu second:%zu third:%zu", 10, 0
 
 .code
 
@@ -27,45 +15,37 @@ engine_initialize proc
 
 	FUNCTION_PROLOGUE
 
-	; Initialize heap
-	sub       rsp, 28h        ; Allocate shadow space and align stack
-	call      heap_initialize ; Initialize heap
-	add       rsp, 28h        ; Restore stack
+	; Allocate shadow space
+	sub       rsp, 20h
 
-	; Initialize console
-	sub       rsp, 28h           ; Allocate shadow space and align stack
-	call      console_initialize ; Initialize console
-	add       rsp, 28h           ; Restore stack
+	; Initialize heap
+	call      heap_initialize ; Initialize heap
+
+	; TODO
+	mov       rcx, 32    ; [ARG0] block_size
+	call      heap_alloc ; Heap alloc
+
+	; TODO
+	mov       rcx, rax  ; [ARG0] block
+	call      heap_free ; Heap free
 
 	; Initialize window
-	sub       rsp, 28h          ; Allocate shadow space and align stack
 	call      window_initialize ; Initialize window
-	add       rsp, 28h          ; Restore stack
 
 	; Create window
-	sub       rsp, 28h      ; Allocate shadow space and align stack
 	call      window_create ; Create window
-	add       rsp, 28h      ; Restore stack
 
 	; Engine loop
-	sub       rsp, 28h    ; Allocate shadow space and align stack
 	call      engine_loop ; Engine loop
-	add       rsp, 28h    ; Restore stack
 
 	; Destroy window
-	sub       rsp, 28h       ; Allocate shadow space and align stack
 	call      window_destroy ; Destroy window
-	add       rsp, 28h       ; Restore stack
-
-	; Restore console
-	sub       rsp, 28h        ; Allocate shadow space and align stack
-	call      console_restore ; Restore console
-	add       rsp, 28h        ; Restore stack
 
 	; Validate heap
-	sub       rsp, 28h      ; Allocate shadow space and align stack
 	call      heap_validate ; Validate heap
-	add       rsp, 28h      ; Restore stack
+
+	; Restore stack
+	add       rsp, 28h
 
 	FUNCTION_EPILOGUE
 
@@ -80,16 +60,22 @@ engine_loop proc
 
 	FUNCTION_PROLOGUE
 
+	; Allocate shadow space
+	sub       rsp, 20h
+
 loop_head:
 
 	; Poll Events
-	sub       rsp, 28h           ; Allocate shadow space and align stack
 	call      window_poll_events ; Poll events
-	add       rsp, 28h           ; Restore stack
 
 	; Check if window has closed
 	cmp       g_window_should_close, 0 ; Compare window should close
 	je        loop_head                ; Continue loop if window should not close
+
+loop_tail:
+
+	; Restore stack
+	add       rsp, 28h
 
 	FUNCTION_EPILOGUE
 
@@ -104,63 +90,17 @@ main proc argc:dword, argv:qword, envp:qword
 
 	FUNCTION_PROLOGUE
 
-	; Initialize C Run-Time
-	sub       rsp, 28h  ; Allocate shadow space and align stack
+	; Allocate shadow space
+	sub       rsp, 20h
+
+	; Initialize CRT
 	call      _CRT_INIT ; Initialize CRT
-	add       rsp, 28h  ; Restore stack
-
-	; Initialize FPU and MXCSR
-	lea       rcx, g_fld_ctrl   ; Default FPU control word
-	fldcw     [rcx]             ; Load FPU control word
-	lea       rdx, g_mxcsr_ctrl ; Default MXCSR control word
-	ldmxcsr   [rdx]             ; Load MXCSR control dword
-
-	; TODO
-	; mov rcx, 32 ; [ARG0] size
-	; call heap_alloc
-
-	; TODO
-	; mov rcx, rax ; [ARG0] block
-	; call heap_free
-
-	; TODO
-	sub       rsp, 28h               ; Allocate shadow space and align stack
-	mov       rdx, 0                 ; [ARG1] num_args
-	lea       rcx, g_format_string_0 ; [ARG0] format
-	call      console_log            ; Console log
-	add       rsp, 28h               ; Restore stack
-
-	; TODO
-	sub       rsp, 28h               ; Allocate shadow space and align stack
-	push      42                     ; [ARG4] variadic
-	mov       rdx, 1                 ; [ARG1] num_args
-	lea       rcx, g_format_string_1 ; [ARG0] format
-	call      console_log            ; Console log
-	add       rsp, 28h               ; Restore stack
-
-	; TODO
-	sub       rsp, 28h               ; Allocate shadow space and align stack
-	push      43                     ; [ARG5] variadic
-	push      42                     ; [ARG4] variadic
-	mov       rdx, 2                 ; [ARG1] num_args
-	lea       rcx, g_format_string_2 ; [ARG0] format
-	call      console_log            ; Console log
-	add       rsp, 28h               ; Restore stack
-
-	; TODO
-	sub       rsp, 28h               ; Allocate shadow space and align stack
-	push      44                     ; [ARG6] variadic
-	push      43                     ; [ARG5] variadic
-	push      42                     ; [ARG4] variadic
-	mov       rdx, 3                 ; [ARG1] num_args
-	lea       rcx, g_format_string_3 ; [ARG0] format
-	call      console_log            ; Console log
-	add       rsp, 28h               ; Restore stack
 
 	; Initialize engine
-	sub       rsp, 28h          ; Allocate shadow space and align stack
-	call      engine_initialize ; Validate heap
-	add       rsp, 28h          ; Restore stack
+	call      engine_initialize ; Initialize engine
+
+	; Restore stack
+	add       rsp, 28h
 
 	FUNCTION_EPILOGUE
 
